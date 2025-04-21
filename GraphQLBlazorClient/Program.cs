8c3b2f4f30e93652a40d073a4f49ef6d8b38f925
mattstrausstrility <matt.strauss@trility.io>
2025-04-21 15:14:45 -0600

    Initial commit

---
+++ GraphQLBlazorClient/Program.cs
@@ -0,0 +1,31 @@
+using BlazorPlayground.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services
+    .AddGraphQLExampleClient()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri("http://localhost:4000"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
