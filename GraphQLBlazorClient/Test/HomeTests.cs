6d5e0a8a67a2f10bc33cc220e2b45ca20ffe9e98
mattstrausstrility <matt.strauss@trility.io>
2025-04-29 12:53:06 -0600

    Adding some unit tests and adding comments

--- GraphQLBlazorClient/Test/HomeTests.cs
+++ GraphQLBlazorClient/Test/HomeTests.cs
@@ -8,9 +8,13 @@ public class HomeTests : PageTest
     [Fact]
     public async Task ShouldGetBooks()
     {
+        // Navigate to the running website
         await Page.GotoAsync("http://localhost:5152");
+        
+        // Click the book query button on the rendered page
         await Page.GetByTestId("get-books-button").ClickAsync();
 
+        // Look for an expected book to appear on the page
         await Expect(Page.GetByText("The Lord of the Rings")).ToBeVisibleAsync();
     }
 
@@ -20,13 +24,18 @@ public class HomeTests : PageTest
         var rand = new Random();
         var title = "Cool Book " + rand.Next();
 
+        // Navigate to the running website
         await Page.GotoAsync("http://localhost:5152");
+        
+        // Fill in the form for adding the book
         await Page.GetByTestId("create-book-title").FillAsync(title);
         await Page.GetByTestId("create-book-author").SelectOptionAsync("JRR Tolkien");
         await Page.GetByTestId("create-book-library").SelectOptionAsync("Memorial Library");
         
+        // Click the add button on the rendered page
         await Page.GetByTestId("create-book-button").ClickAsync();
-
+        
+        // Look for an expected book to appear on the page
         await Expect(Page.GetByText(title)).ToBeVisibleAsync();
     }
 }
